d446083f737b1e00e038f505da7a8cc05c65437f
Dima Rogov <ds.rogv@gmail.com>
2019-11-30 21:43:34 +0300

    Add Question model with comments

--- db/schema.rb
+++ db/schema.rb
@@ -15,20 +15,10 @@ ActiveRecord::Schema.define(version: 2019_11_30_171837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.text "body"
-    t.bigint "question_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-  end
-
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_foreign_key "answers", "questions"
 end
--- spec/models/answer_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-describe Answer do
-  it { is_expected.to belong_to(:question) }
-  it { is_expected.to validate_presence_of(:body) }
-end
